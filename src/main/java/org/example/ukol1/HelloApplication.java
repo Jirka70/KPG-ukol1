8b42248318766ddcafbbfcae6b97a92e6552a60f
Jirka70 <jiritresohlavy2@gmail.com>
2024-02-14 17:31:26 +0100

    displayed window

--- src/main/java/org/example/ukol1/HelloApplication.java
+++ src/main/java/org/example/ukol1/HelloApplication.java
@@ -1,19 +1,19 @@
 package org.example.ukol1;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
+    private static final double WIDTH = 800;
+    private static final double HEIGHT = 600;
+    private final Pane mainPane = new Pane();
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) {
+        Scene scene = new Scene(mainPane, WIDTH, HEIGHT);
         stage.setScene(scene);
+        stage.setTitle("KPG - úkol č.1 A22B0106P");
         stage.show();
     }
 
